15842cdbd08e18a4d2c74c2cbb3ad02b8196dd29
Giel Jurriëns <gieldid@gmail.com>
2021-12-01 17:29:14 +0100

    rs232 communication line between scale and raspberry pi

--- WeightScaleReader.js
+++ WeightScaleReader.js
@@ -1,23 +1,35 @@
-const bindings = require("@serialport/bindings");
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
 
-/**
- * Get a list of available serial ports. 
- * @param {boolean} verbose - Log results if true. 
- * @returns {{status: string, data: array|object}} - 'ok' or 'fail' with details to handle elsewhere. 
- */
-const listPorts = async (verbose) => {
-  let result;
-  try {
-    const portList = await bindings.list();
-    if (verbose) console.table(portList); // Print out the array if desired.
-    result = { status: "ok", data: portList };
-  } catch (err) {
-    if (verbose) console.log(err); // To see what the error is, if desired.
-    result = { status: "fail", data: err };
+const port = new SerialPort('/dev/ttyUSB0', {
+  baudRate: 9600,
+  dataBits: 8,
+  stopBits: 1,
+  parity: "none"
+})
+
+// port.on('readable', function () {
+//   console.log('Data:', port.read())
+// })
+
+// // Switches the port into "flowing mode/"
+// port.on('data', function (data) {
+//   console.log('Data:', data)
+// })
+
+// Pipe the data into another stream (like a parser or standard out)
+const parser = port.pipe(new Readline({ delimiter: '\r\n' }))
+parser.on('data', function (data){
+  if(data.includes("W:+")){
+    data = data.replace('W:+','')
+    data = data.replace('kg','')
+    var weightInGrams = data.replace('.','')
+    if(weightInGrams != 000){
+      console.log(weightInGrams)
+      port.close(function (err) {
+        console.log('port closed', err);
+      });
+    }
   }
-  return result;
-};
+})
 
-// Run the command.
-const { portStatus, portList } = listPorts(true);
-// If portStatus is 'ok' then portsList is ready to use. 
