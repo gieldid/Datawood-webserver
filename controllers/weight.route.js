aa0658e8de5b42d43717034e99f9754135f3c062
Giel Jurriëns <gieldid@gmail.com>
2021-12-03 01:21:27 +0100

    Error logging for when no device is connected

--- controllers/weight.route.js
+++ controllers/weight.route.js
@@ -4,12 +4,17 @@ const weightModel = require("../models/weight.model");
 const weigthScaleReader = require("../utils/scale/WeightScaleReader");
 
 router.get("/", async function (request, response) {
-    //execute task to get weight here 
+    //execute task to get weight here
+
     let weightOnScale = await weigthScaleReader.getWeight();
-    await weightModel.insertWeight(weightOnScale);
-    let weightData = await weightModel.last();
-    
-    response.json({weight: weightData.weight});
+    console.log("weightonscale; " + weightOnScale);
+    if(weightOnScale === "Failed to connect to rs232 device" || !weightOnScale){
+        response.send("Failed to connect to rs232 device");
+    }else{
+        await weightModel.insertWeight(weightOnScale);
+        let weightData = await weightModel.last();
+        response.json({weight: weightData.weight});
+    }
 });
 
 module.exports = router;
--- utils/scale/WeightScaleReader.js
+++ utils/scale/WeightScaleReader.js
@@ -1,39 +1,39 @@
-const SerialPort = require('serialport')
-const Readline = require('@serialport/parser-readline')
+const SerialPort = require('serialport');
+const Readline = require('@serialport/parser-readline');
 
 module.exports = {
   async getWeight(){
-    const port = new SerialPort('/dev/ttyUSB0', {
-      baudRate: 9600,
-      dataBits: 8,
-      stopBits: 1,
-      parity: "none"
-    })
-
-    // port.on('readable', function () {
-    //   console.log('Data:', port.read())
-    // })
-
-    // // Switches the port into "flowing mode/"
-    // port.on('data', function (data) {
-    //   console.log('Data:', data)
-    // })
-
-    // Pipe the data into another stream (like a parser or standard out)
-    const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
-    parser.on('data', function (data){
-      if(data.includes("W:+")){
-        data = data.replace('W:+','')
-        data = data.replace('kg','')
-        var weightInGrams = data.replace('.','')
-        if(weightInGrams != 000){
-
-          console.log(weightInGrams)
-          port.close(function (err) {
-            return weightInGrams
-          })
+    try {
+      const port = new SerialPort('/dev/ttyUSB0', {
+        baudRate: 9600,
+        dataBits: 8,
+        stopBits: 1,
+        parity: "none"
+      },function(err){
+        if(err){
+          return "Failed to connect to rs232 device";
         }
-      }
-    })
+      }); 
+
+      const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+
+      parser.on('data', function (data){
+        if(data.includes("W:+")){
+          data = data.replace('W:+','');
+          data = data.replace('kg','');
+          var weightInGrams = data.replace('.','');
+          if(weightInGrams != 000){
+  
+            console.log(weightInGrams);
+            port.close(function (err) {
+              return weightInGrams;
+            })
+          }
+        }
+      }) 
+
+    } catch (error) {
+      throw new Error("Failed to connect to rs232 device");
+    }
   }
 }
